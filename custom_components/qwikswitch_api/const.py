c018979bb65b98168d9037a8feaf8a38f15b671f
Riaan Hanekom <riaan@hanekom.io>
2025-02-01 16:36:22 +0000

    Update polling to be default 5s.

--- custom_components/qwikswitch_api/const.py
+++ custom_components/qwikswitch_api/const.py
@@ -9,7 +9,7 @@ DOMAIN: Final = "qwikswitch_api"
 
 CONF_MASTER_KEY: Final = "master_key"
 CONF_POLL_FREQUENCY = "poll_frequency"
-DEFAULT_POLL_FREQUENCY: int = 3  # seconds
+DEFAULT_POLL_FREQUENCY: int = 5  # seconds
 
 DATA_QS_CLIENT: str = "qs_api_client"
 DATA_QS_COORDINATOR: str = "qs__api_coordinator"
